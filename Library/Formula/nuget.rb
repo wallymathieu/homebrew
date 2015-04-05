d559a30b763d4b539080a8872a19dc7a7cae2ad2
Oskar Gewalli <oskar@gewalli.se>
2015-04-05 21:08:40 +0200

    depends on mono

--- Library/Formula/nuget.rb
+++ Library/Formula/nuget.rb
@@ -4,6 +4,8 @@ class Nuget < Formula
   sha256 "6e0455bbae41f117c22b080988c48240e58448d7b5583659afba2b7873f3df39"
   version "2.8.60318.667"
 
+  depends_on "mono"
+
   def install
     (share/'tool').install 'nuget.exe'
     (bin/'nuget').write <<-EOS
